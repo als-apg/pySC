ca7fc2debc21dce2320675bccc3e1809962b7259
kparasch <konstantinos.paraschou@desy.de>
2026-06-04 11:41:59 +0200

    multipolar config improvement, allows inline specification of imperfection models.

--- pySC/configuration/multipolar_imperfections_conf.py
+++ pySC/configuration/multipolar_imperfections_conf.py
@@ -4,13 +4,26 @@ from ..core.multipolar_imperfections import ImperfectionsModelFactory
 
 logger = logging.getLogger(__name__)
 
-def expand_multipolar_imperfections_tables(config_dict: dict):
-    tables = config_dict['multipolar_imperfection_models']
-    for table_name in tables:
-        table_file = tables[table_name]
-        logger.info(f"Loading MultipolarImperfectionTable in {table_file}.")
-        tables[table_name] = load_yaml(table_file)
+def expand_multipolar_imperfection_models(config_dict: dict):
+    models = config_dict['multipolar_imperfection_models']
 
-        # pre-validate tables to catch errors just after loading.
-        # Validation will be redone again.
-        ImperfectionsModelFactory.model_validate({'factories': tables[table_name]})
+    for model_name, model_spec in models.items():
+        if isinstance(model_spec, str):
+            logger.info(
+                f"Loading multipolar imperfection model {model_name} from {model_spec}."
+            )
+            model_object = load_yaml(model_spec)
+        else:
+            model_object = model_spec
+
+        if not isinstance(model_object, list):
+            raise TypeError(
+                f"Multipolar imperfection model {model_name} must be a list of "
+                f"table/curve entries or a filename pointing to such a list. "
+                f"Got {type(model_object)}."
+            )
+
+        # Pre-validate models to catch errors just after loading.
+        # Validation will be redone later.
+        ImperfectionsModelFactory.model_validate({"factories": model_object})
+        models[model_name] = model_object
